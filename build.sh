c7345de588f344f8a7ef66424ba3a474bcfeb282
Sabonaterefe <sabonaterefe@example.com>
2025-12-05 09:13:28 +0300

    Fix tailwind build command

--- build.sh
+++ build.sh
@@ -6,12 +6,13 @@ set -o errexit
 pip install -r requirements.txt
 
 # 2. Install Node.js & Build Tailwind CSS
-# This step is CRITICAL for your style to work
+# (We removed --no-input from these two lines)
 python -m pip install django-tailwind
-python manage.py tailwind install --no-input
-python manage.py tailwind build --no-input
+python manage.py tailwind install
+python manage.py tailwind build
 
 # 3. Collect all static files (images/css)
+# (Keep --no-input here, it is correct for this one)
 python manage.py collectstatic --no-input
 
 # 4. Set up the database
