37dafc5e8733c62b8a64366b9c278c05d4f99cac
Sabonaterefe <sabonaterefe@example.com>
2025-12-05 09:44:07 +0300

    Force git to upload static folder

--- build.sh
+++ build.sh
@@ -5,14 +5,25 @@ set -o errexit
 # 1. Install Python packages
 pip install -r requirements.txt
 
-# 2. Install Node.js & Build Tailwind CSS
+# 2. Create the missing static folder (Crucial Fix)
+# This prevents Django from complaining if the folder is missing from Git
+mkdir -p static
+mkdir -p staticfiles
+
+# 3. Install Node.js & Build Tailwind CSS
 python -m pip install django-tailwind
 python manage.py tailwind install
 python manage.py tailwind build
 
-# 3. Collect all static files (images/css)
-# Added --clear to ensure we start fresh and get the new tailwind build
+# 4. NUCLEAR RESET of Static Files
+# We delete the destination folder completely to force a fresh copy
+rm -rf staticfiles
+mkdir staticfiles
+
+# 5. Collect static files
+# We use --no-input to prevent prompts
+# We use --clear to be double safe
 python manage.py collectstatic --no-input --clear
 
-# 4. Set up the database
+# 6. Set up the database
 python manage.py migrate
